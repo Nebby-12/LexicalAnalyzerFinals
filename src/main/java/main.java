cc3dc528eec284e6ac4447b912f6ac4041c697d3
Nebby-12 <benvllcrt@gmail.com>
2022-07-07 16:37:11 +0800

    alphanum
    gumawa ng alpha num at sinimulan yuung LexicalSolver

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,26 @@
 public class main {
+
+    public static boolean isalnum(char val){
+        return (val >= 'a' && val <= 'z') || (val >= 'A' && val <= 'Z') || (val >= '0' && val <= '9');
+    }
+
+    public void lexicalSolver(char word[]){
+        int start = 0;
+        int end = 0;
+        int peek = 0;
+        String value = "";
+
+        for(int i = 0; i< word.length; i++){
+            start = i;
+            end = i;
+            if (isalnum(word[i])) {
+                peek = end + 1;
+                if 
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
